5fab780af8f0d1bd5d40f4d68c92b91ac1c1c17b
smyarkann <82476735+smyarkann@users.noreply.github.com>
2023-05-17 22:38:23 +0300

    react
    react proje

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {BrowserRouter as Router, Route, Routes, Link, Outlet, Navigate} from 'react-router-dom';
+import Login from './Login';
+import Register from './Register';
+import Portal from './Portal';
+import BasvuruGoruntule from './BasvuruGoruntule';
+import BasvuruFormu from './BasvuruFormu';
+import IletisimBilgileri from './IletisimBilgileri';
+import DokumanYukleme from './DokumanYukleme';
+import BilgiGuncelle from './BilgiGuncelle';
+import BasvuruSorgulama from './BasvuruSorgulama';
+import Sifredegistirme from './Sifredegistirme';
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <Router>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/kayit" element={<Register />} />
+      <Route path="/portal" element={<Portal />} />
+      <Route path="/portal/BasvuruFormu" element={<BasvuruFormu/>} />
+      <Route path="/portal/BasvuruGoruntule" element={<BasvuruGoruntule/>} />
+      <Route path="/portal/IletisimBilgileri" element={<IletisimBilgileri/>}/>
+      <Route path="/portal/DokumanYukleme" element={<DokumanYukleme/>}/>
+      <Route path="/portal/BilgiGuncelle" element={<BilgiGuncelle/>}/>
+      <Route path="/portal/BasvuruSorgulama" element={<BasvuruSorgulama/>}/>
+      <Route path="/portal/Sifredegistirme" element={<Sifredegistirme/>}/>
+    </Routes>
+  </Router>
+
   );
 }
 
